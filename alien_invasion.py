6b0cea688e55d2304032614c3de13d4042ec4abf
markose1 <mesaymarkose@gmail.com>
2023-04-16 19:18:13 -0500

    Commit 5

--- alien_invasion.py
+++ alien_invasion.py
@@ -117,7 +117,7 @@ class AlienInvasion:
         current_x,current_y = alien_width, alien_height
         while current_y < (self.settings.screen_width - 3 * alien_width):
             while current_x < (self.settings.screen_width - 2 * alien_width):
-                self._create_alien(current_x)
+                self._create_alien(current_x,current_y)
                 current_x += 2 * alien_width
             #Finished a row; reset x value, and icrement y value.
             current_x = alien_width
